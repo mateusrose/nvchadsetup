d4e086b88dbe054af9c9d2ea842d198810d8017f
mateusrosa <pedrompmrosa@gmail.com>
2024-10-29 18:03:28 +0000

    commitedbutton

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,10 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+map("n", "<leader>fv", function()
+    require("telescope.builtin").live_grep({ grep_open_files = true })
+end, { desc = "Search in all open buffers" })
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
