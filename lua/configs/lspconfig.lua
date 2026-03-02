b029cec0a198c7881f5b9ec8bf3bd0171cb31f49
Pedro Mateus Rosa <pedro.rosa@caixamagica.pt>
2026-03-02 12:25:06 +0000

    feat(lsp): enable ts ls for backend

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,63 @@
+-- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+local lspconfig = vim.lsp.config;
+-- EXAMPLE
+local servers = { "html", "cssls", "pyright", "jdtls", "tailwindcss", 'ts_ls', 'eslint' }
+local nvlsp = require "nvchad.configs.lspconfig"
 
--- read :h vim.lsp.config for changing options of lsp servers 
+lspconfig('ts_ls',
+  {
+    filetypes = {"javascript", "javascriptreact"}
+  }
+)
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig('jsonls', {
+  capabilities = capabilities,
+  cmd = { "vscode-json-language-server", "--stdio" },
+  filetypes = { "json", "jsonc" },
+  init_options = {
+    provideFormatter = true,
+  },
+  root_markers = { ".git" },
+})
+
+vim.lsp.enable('jsonls')
+
+lspconfig('pyright', {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    python = {
+      pythonPath = '/home/mateusrosa/.pyenv/versions/3.8.20/bin/python'
+    }
+  }
+})
+-- lsps with default config
+ for _, lsp in ipairs(servers) do
+   if lsp ~= "pyright" then
+     lspconfig(lsp, {
+       on_attach = nvlsp.on_attach,
+       on_init = nvlsp.on_init,
+     capabilities = nvlsp.capabilities,
+     })
+  end
+end
+
+vim.lsp.enable('pyright')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('jsonls')
+vim.lsp.enable('html')
+vim.lsp.enable('cssls')
+vim.lsp.enable('jdtls')
+vim.lsp.enable('eslint')
+-- configuring single server, example: typescript
+-- lspconfig.tsserver.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
